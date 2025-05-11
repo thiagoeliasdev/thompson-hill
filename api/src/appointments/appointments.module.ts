736963891e428a9fb55491a35cf7442a0afc3b8f
Thiago Elias <thiagoelias99@gmail.com>
2025-05-11 19:22:04 -0300

    fix modules

--- api/src/appointments/appointments.module.ts
+++ api/src/appointments/appointments.module.ts
@@ -3,10 +3,11 @@ import { AppointmentsService } from './appointments.service'
 import { AppointmentsController } from './appointments.controller'
 import { CustomersModule } from "src/customers/customers.module"
 import { ServicesModule } from "src/services/services.module"
+import { UsersModule } from "src/users/users.module"
 
 @Module({
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
-  imports: [CustomersModule, ServicesModule]
+  imports: [CustomersModule, ServicesModule, UsersModule]
 })
 export class AppointmentsModule { }
